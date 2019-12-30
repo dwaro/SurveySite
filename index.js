35031ae38cedd2d7259425985abc3a4bb3384f7c
Jacob Waro <davidjwaro@gmail.com>
2019-12-30 17:09:18 -0600

    Finish OAuth build

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oath20").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express(); // create express app
 
 // create new instance of Google Passport Strategy
 // passport.use --> telling passport that there is a new strategy available
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token:", accessToken);
+      console.log("refresh token:", refreshToken);
+      console.log("profile:", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"] //asking google for access to user's profile and email info
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // create first route handler
 // this was really just a test route handler
