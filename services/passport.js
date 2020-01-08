b45f45fd1c41adda06c5e830519909b6006a2b77
Jacob Waro <davidjwaro@gmail.com>
2020-01-08 14:23:11 -0600

    refactor

--- services/passport.js
+++ services/passport.js
@@ -25,18 +25,17 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a user with this id.
-          done(null, existingUser);
-        } else {
-          // we do not have a user with this id.
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // we already have a user with this id.
+        return done(null, existingUser);
+      }
+
+      // we do not have a user with this id.
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
