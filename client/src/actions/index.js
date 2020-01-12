8f7120a27037e18ca86196f561dcbb28f4163816
Jacob Waro <davidjwaro@gmail.com>
2020-01-12 16:50:19 -0600

    Handling Payments

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,11 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get("api/current_user");
+  const res = await axios.get("api/current_user"); // get retrieves data from a server
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post("/api/stripe", token); // post asks a server to accept data
   dispatch({ type: FETCH_USER, payload: res.data });
 };
