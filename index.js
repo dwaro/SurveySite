c5028ae850e5df9d7091f2e2329f16af66af056c
Jacob Waro <davidjwaro@gmail.com>
2020-01-14 19:11:53 -0600

    Added billing and client side app

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets like main.js or main.css files
+  app.use(express.static("client/build"));
+
+  // Express will serve up index.html file if it does not recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 /*  instructs express to tell Node that it wants to listen for incoming traffic
  *  on PORT. Node is actually the one listening for the request. If process.env.PORT
  *  exists, then we will use that (i.e. the port Heroku tells us), otherwise,
