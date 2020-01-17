99a2ebe7169ff5cb2f390bc0187d50b2751b8647
Jacob Waro <davidjwaro@gmail.com>
2020-01-17 13:03:53 -0600

    Start setting up SendGrid

--- models/survey.js
+++ models/survey.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose; // == const Schema = mongoose.Schema;
+const RecipientSchema = require("./Recipient");
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String], // comma separated list of strings
+  recipients: [RecipientSchema], // sub-document collection
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: "User" },
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model("surveys", surveySchema);
