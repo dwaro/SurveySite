9e650c936b242ecf907d0ae815ffd2c4cce41164
Jacob Waro <davidjwaro@gmail.com>
2020-01-23 10:01:04 -0600

    Start forms on Client

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains logic to render a single label and render input
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
