67f527c4771aa58108c114511d18ef7b9aaa04b8
Jacob Waro <davidjwaro@gmail.com>
2020-01-28 09:58:12 -0600

    finish client forms

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,25 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
+// export const changeLogin = shouldBeLoggedIn => {
+//   return {
+//     type: "change_auth",
+//     payload: shouldBeLoggedIn
+//   };
+// };
+
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get("api/current_user"); // get retrieves data from a server
+  let res = await axios.get("/api/current_user"); // get retrieves data from a server
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = token => async dispatch => {
   const res = await axios.post("/api/stripe", token); // post asks a server to accept data
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post("/api/surveys", values);
+  history.push("/surveys");
   dispatch({ type: FETCH_USER, payload: res.data });
 };
