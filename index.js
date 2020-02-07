3034e565be1b1005f1a028d00e27d898c7ec746f
Jacob Waro <davidjwaro@gmail.com>
2020-02-07 11:28:34 -0600

    Heroku module-not-found fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
-require("./models/Survey");
+require("./models/Surveys");
 require("./models/User");
 require("./services/passport");
 
